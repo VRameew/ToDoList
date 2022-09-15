32810810e0cbbb7bd8203b5bf164a289b65cf4ad
v.rameew <v.rameew@gmail.com>
2022-09-15 21:22:52 +0300

    Включили приложение lists в настройках, был произведен рефакторинг тестов на проверку шаблонов, был введен шаблон начальной страницы

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 
 class HomePageTest(TestCase):
@@ -12,9 +13,5 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         """Test for valid HTML"""
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        response = self.client.get('')
+        self.assertTemplateUsed(response, 'home.html')
