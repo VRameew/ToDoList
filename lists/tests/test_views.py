782411033b07688b68125eeab7167bb5b9639f57
v.rameew <v.rameew@gmail.com>
2022-11-08 19:14:42 +0300

    Рефакторизовано представление списка для обработки POSTзапросов с новыми элементами

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -63,7 +63,7 @@ class ListViewTest(TestCase):
     def test_validation_errors_end_up_on_lists_page(self):
         list_ = List.objects.create()
         response = self.client.post(
-            f"/list/{list_.id}/",
+            f"/lists/{list_.id}/",
             data={'item_text': ''}
         )
         self.assertEqual(response.status_code, 200)
