a9ab86c4fdd46f23361e343fe4a3f742afb24e7b
v.rameew <v.rameew@gmail.com>
2022-11-08 19:10:32 +0300

    Рефакторизовано представление списка для обработки POSTзапросов с новыми элементами

--- lists/urls.py
+++ lists/urls.py
@@ -17,6 +17,6 @@ from django.urls import path
 from lists import views
 
 urlpatterns = [
-    path('/list/<int:list_id>/', views.view_list, name='view_list'),
+    path('<int:list_id>/', views.view_list, name='view_list'),
     path('new', views.new_list, name='new_list'),
 ]
