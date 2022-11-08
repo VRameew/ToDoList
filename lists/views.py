e255f35d42b9d57f92d11daf1393e72b8bdf0bf8
v.rameew <v.rameew@gmail.com>
2022-11-08 19:00:57 +0300

    Рефакторизовано представление списка для обработки POSTзапросов с новыми элементами

--- lists/views.py
+++ lists/views.py
@@ -16,7 +16,7 @@ def view_list(request, list_id):
             item = Item(text=request.POST['item_text'], list=list_)
             item.full_clean()
             item.save()
-            return redirect(f"/lists/{list_.id}")
+            return redirect(list_)
         except ValidationError:
             error = "You can't have an empty list item"
     return render(request, 'list.html', {'list': list_, 'error': error})
@@ -24,7 +24,7 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['item_text'], list=list_)
     try:
         item.full_clean()
         item.save()
@@ -32,4 +32,4 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
-    return redirect(f'/lists/{list_.id}/')
+    return redirect(list_)
