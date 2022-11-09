7b620dfdfc428c8c575c235bdbd3cce5940f23cb
v.rameew <v.rameew@gmail.com>
2022-11-09 17:49:04 +0300

    .

--- lists/forms.py
+++ lists/forms.py
@@ -3,4 +3,10 @@ from django import forms
 
 class ItemForm(forms.Form):
     """Form element of list"""
-    item_text = forms.CharField()
+    item_text = forms.CharField(
+        widget=forms.fields.TextInput(
+            attrs={
+                'placeholder': 'Enter a to-do item',
+            }
+        ),
+    )
