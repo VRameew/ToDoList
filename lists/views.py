51b9af09644e05736bae89040f6fbddebef656a9
v.rameew <v.rameew@gmail.com>
2022-11-23 20:08:06 +0300

    Вывели валидацию через формы, тесты все успешны

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 from django.shortcuts import render, redirect
 from lists.models import Item, List
-from lists.forms import ItemForm
+from lists.forms import ExistingListItemForm, ItemForm
 
 
 def home_page(request):
@@ -11,13 +11,13 @@ def home_page(request):
 def view_list(request, list_id):
     """Rendering list view"""
     list_ = List.objects.get(id=list_id)
-    form = ItemForm()
+    form = ExistingListItemForm(for_list=list_)
     if request.method == "POST":
-        form = ItemForm(data=request.POST)
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
         if form.is_valid():
-            form.save(for_list=list_)
+            form.save()
             return redirect(list_)
-    return render(request, 'list.html', {'list': list_, 'form': form})
+    return render(request, 'list.html', {'list': list_, "form": form})
 
 
 def new_list(request):
@@ -25,7 +25,8 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        form.save(for_list=list_)
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
+        form.save()
         return redirect(list_)
     else:
         return render(request, 'home.html', {'form': form})
